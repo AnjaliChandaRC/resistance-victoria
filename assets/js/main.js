40b439a441f160b93b9528c32ea2d3c0c8c0ae2b
tusharswami534 <tusharswami17@gmail.com>
2024-10-25 12:37:39 +0530

    World Slider done

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,29 @@ function topFunction() {
 }
 
 document.getElementById("vid").play();
+// YOUR WORLD GAME BOARD SLICK SLIDER 
+
+$('.world-game-slider').slick({
+  dots: true,
+  infinite: true,
+  variableWidth: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  nextArrow: ".next",
+  prevArrow: ".prev",
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
