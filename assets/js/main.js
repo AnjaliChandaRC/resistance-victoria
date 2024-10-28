c3f4484178adad392e3877ab9e494fb508434914
anjali <anjalichanda2019@gmail.com>
2024-10-28 11:12:11 +0530

    remove extra code

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,30 +43,16 @@ function topFunction() {
 document.getElementById("vid").play();
 
 // YOUR WORLD GAME BOARD SLICK SLIDER 
-
 $('.world-game-slider').slick({
   dots: true,
   infinite: true,
-  variableWidth: true,
   speed: 1000,
   slidesToShow: 1,
   slidesToScroll: 1,
   centerMode: true,
   variableWidth: true,
-  // autoplay: true,
-  // autoplaySpeed: 1000,
+  autoplay: true,
+  autoplaySpeed: 1000,
   nextArrow: ".next",
   prevArrow: ".prev",
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
 });
